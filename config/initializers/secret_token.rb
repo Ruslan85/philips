b72731531ddb375de76d2aa8b2521802309f4164
Mulyukov Ruslan <ret8558@mail.ru>
2014-03-10 10:40:05 +0400

    jQuery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Philips::Application.config.secret_key_base = '86cf9d33c00ed863dbe04836e40a25231bdcdac00dcda74907e13cda11c9c8027c0d2d13060fa3e76f8dbe62a7d25bfc3eafb492e16d26f274c752035f2c7577'
